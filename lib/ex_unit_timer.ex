480d77da2134894176bc6162f11d89a0c6f1fed4
Paul Infield-Harm <paul@infield-harm.org>
2015-07-20 12:34:03 -0400

    Generally working

--- lib/ex_unit_timer.ex
+++ lib/ex_unit_timer.ex
@@ -1,7 +1,3 @@
 defmodule ExUnitTimer do
-  def initialize do
-    IO.puts "hello!"
-    ExUnit.configure(formatters: [ExUnitTimer.TimeFormatter|Dict.get(ExUnit.configuration, :formatters)])
-  end
 end
 
--- lib/ex_unit_timer/time_formatter.ex
+++ lib/ex_unit_timer/time_formatter.ex
@@ -1,5 +1,5 @@
 defmodule ExUnitTimer.TimeFormatter do
-  
+  require Logger
   use GenEvent
   
   def init(opts) do
@@ -21,13 +21,31 @@ defmodule ExUnitTimer.TimeFormatter do
   end
 
   defp print_time_reports(results) do
-    sorted = Enum.sort(results, fn (a,b) -> a.time > b.time end)
-    time_width = String.length("#{hd(sorted)}")
-    name_width = Enum.max(Enum.map(results, fn(r) -> String.length(r.name) end))
-    case_width = Enum.max(Enum.map(results, fn(r) -> String.length(r.case) end))
     
-    Enum.each(sorted, fn(t) -> :io.format("~#{time_width}s | ~#{case_width}s | ~#{name_width}", ["#{t.time}", "#{t.case}", "#{t.name}"]) end) 
+    sorted = Enum.sort(results, fn (a,b) -> a.time < b.time end)
+    time_width = String.length("#{List.last(sorted).time}")
+    
+    mapped = Enum.map(sorted, &([&1.time, relative_location(&1), String.replace("#{&1.name}", ~r/test /,"", global: false)]))
+    path_width = Enum.max(Enum.map(mapped, fn [_, path, _] -> String.length(path) end))
+    name_width = max(terminal_width - (time_width + path_width + 6), 20)
+    Enum.each(mapped, fn [time, path, name] -> :io.format("~#{time_width}s | ~#{path_width}s | ~-#{name_width}.#{name_width}s~n", ["#{time}", "#{path}", "#{name}"]) end) 
 
   end
+
+  defp terminal_width do
+    try do
+      case System.cmd("tput", ["cols"]) do
+        {w, 0} -> elem(Integer.parse(w),0)
+        x -> 80
+      end
+    catch
+      _ -> 80
+    end
+  end
+
+  defp relative_location(test) do
+    "#{Path.relative_to_cwd(test.tags[:file])}:#{test.tags[:line]}"
+  end
+
 
 end
