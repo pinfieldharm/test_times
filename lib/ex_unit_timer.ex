c31ad9f289e7d1c541292160c3958b75753c27c6
Paul Infield-Harm <paul@infield-harm.org>
2015-07-19 20:41:01 -0400

    Basic skeleton

--- lib/ex_unit_timer.ex
+++ lib/ex_unit_timer.ex
@@ -1,2 +1,7 @@
 defmodule ExUnitTimer do
+  def initialize do
+    IO.puts "hello!"
+    ExUnit.configure(formatters: [ExUnitTimer.TimeFormatter|Dict.get(ExUnit.configuration, :formatters)])
+  end
 end
+
---
+++ lib/ex_unit_timer/time_formatter.ex
@@ -0,0 +1,7 @@
+defmodule ExUnitTimer.TimeFormatter do
+  import ExUnit.Formatter
+
+  def init(opts) do
+    IO.puts("Hola!")
+  end
+end
