4a5ab548eb25a9bc9a7aefae2c120aa03d611f0f
Paul Infield-Harm <paul@infield-harm.org>
2015-07-21 09:24:39 -0400

    Add setup

--- lib/ex_unit_timer.ex
+++ lib/ex_unit_timer.ex
@@ -1,3 +1,13 @@
 defmodule ExUnitTimer do
+
+  def setup do
+    ExUnit.configure(
+      add_formatter(ExUnit.configuration, ExUnitTimer.TimeFormatter)
+    )
+  end
+
+  defp add_formatter(config, f) do
+    Keyword.update(config, :formatters, [f], &([f|&1]))
+  end
 end
 
